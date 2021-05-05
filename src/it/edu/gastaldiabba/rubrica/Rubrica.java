667187d439414ea976ba8f126f9310f830468105
angis <angis@DESKTOP-7U997LF.home>
2021-05-05 17:28:23 +0200

    ultime modifiche

--- src/it/edu/gastaldiabba/rubrica/Rubrica.java
+++ src/it/edu/gastaldiabba/rubrica/Rubrica.java
@@ -21,7 +21,13 @@ import javafx.stage.Stage;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -42,7 +48,7 @@ public class Rubrica extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/Rubrica.fxml"));
+        
         
         try {
             
@@ -51,10 +57,13 @@ public class Rubrica extends Application {
             Document document = build.parse(xmlFilePath());
             File f=new File(xmlFilePath());
             if(f.exists()){
-            leggiXML(document);
+                leggiXML(document);
+                
             }
             else{
-                LeggiScriviXml.creaXML();
+                creaXML();
+                leggiXML(document);
+                
             }
         
         }
@@ -63,6 +72,7 @@ public class Rubrica extends Application {
         
             System.out.println("Errore");
         }
+        Parent root = FXMLLoader.load(getClass().getResource("view/Rubrica.fxml"));
         
         Scene scene = new Scene(root);
        // stage.getIcons().add(new Image(""));
@@ -93,11 +103,20 @@ public class Rubrica extends Application {
         int aff;
         try{
             aff = Integer.parseInt(a);
+            if(aff>=0 && aff<=10){
+                System.out.println("Correct");
+            }
+            else{
+                //finestra che segnala errore
+            }
         } catch(NumberFormatException e){
             aff = 0;
         }
+        System.out.println("lettura corretta");
         Cliente z=new Cliente(nomeAz, indirizzo, cap, telefono, pIVA, citta, mail, aff);
+        System.out.println("cliente creato");
         listaClienti.add(z);
+        System.out.println("cliente aggiunto alla lista");
       }
 
   }
@@ -106,5 +125,71 @@ public class Rubrica extends Application {
         String x = p+"rubrica.xml";
         return x;
     }
+    public static void creaXML() throws ParserConfigurationException, TransformerConfigurationException, TransformerException, SAXException, IOException {
+    
+    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+    Document doc = docBuilder.newDocument();
+    
+    Element rubrica = doc.createElement("Rubrica");
+    doc.appendChild(rubrica);
+    
+    Element cliente = doc.createElement("Cliente");
+    Attr a = doc.createAttribute("affidabilita");
+    a.setValue("10");
+    cliente.setAttributeNode(a);
+    rubrica.appendChild(cliente);
+    
+    Element nomeAzienda = doc.createElement("nomeAzienda");
+    nomeAzienda.appendChild(doc.createTextNode("Microsoft"));
+    cliente.appendChild(nomeAzienda);
+    
+    Element indirizzo = doc.createElement("indirizzo");
+    indirizzo.appendChild(doc.createTextNode("Oxford Street 26/2"));
+    cliente.appendChild(indirizzo);
+    
+    Element cap = doc.createElement("cap");
+    cap.appendChild(doc.createTextNode("03456"));
+    cliente.appendChild(cap);
+    
+    Element citta = doc.createElement("citta");
+    citta.appendChild(doc.createTextNode("London"));
+    cliente.appendChild(citta);
+    
+    Element partitaIVA = doc.createElement("partitaIVA");
+    partitaIVA.appendChild(doc.createTextNode("010386534576"));
+    cliente.appendChild(partitaIVA);
+    
+    Element telefono = doc.createElement("telefono");
+    telefono.appendChild(doc.createTextNode("8034678342"));
+    cliente.appendChild(telefono);
+    
+    Element mail = doc.createElement("mail");
+    mail.appendChild(doc.createTextNode("microsoft@billgates.com"));
+    cliente.appendChild(mail);
+    
+    Element note = doc.createElement("note");
+    note.appendChild(doc.createTextNode("Note di <nome cliente>"));
+    cliente.appendChild(note);
+    
+    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    Transformer transformer = transformerFactory.newTransformer();
+    DOMSource source = new DOMSource(doc);
+    
+    StreamResult streamResult = new StreamResult (new File(xmlFilePath()));
+    transformer.transform(source, streamResult);
+    
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    DocumentBuilder build = dbf.newDocumentBuilder();
+    Document document = build.parse(xmlFilePath());
+      
+    TransformerFactory tf = TransformerFactory.newInstance();
+    Transformer t = tf.newTransformer();
+    DOMSource src = new DOMSource (doc);
+    StreamResult sr = new StreamResult (new File (xmlFilePath()));
+    transformer.transform(src, sr);
+    }
+    
+    
    
 }
--- src/it/edu/gastaldiabba/rubrica/controller/RubricaController.java
+++ src/it/edu/gastaldiabba/rubrica/controller/RubricaController.java
@@ -5,40 +5,43 @@
  */
 package it.edu.gastaldiabba.rubrica.controller;
 
+import it.edu.gastaldiabba.rubrica.Rubrica;
+import it.edu.gastaldiabba.rubrica.model.Cliente;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 
 /**
  *
  * @author angis
  */
-public class RubricaController implements Initializable {
+public class RubricaController extends Rubrica implements Initializable {
     
     ObservableList<String> filterList= FXCollections
             .observableArrayList("Ragione sociale", "Indirizzo", "CAP", "E-mail", "Telefono", "Partita IVA", "Città", "Affidabilità");
+    
     @FXML
     private TextArea src;
     @FXML
     private ComboBox filterBox;
     @FXML
     private AnchorPane sfondo;
-    @FXML
-    private ScrollPane lista;
     @FXML
     private Button aggiungi;
     @FXML
@@ -53,12 +56,57 @@ public class RubricaController implements Initializable {
     private RadioButton decrescente;
     @FXML
     private Button login;
-    
+    @FXML
+    private Label label;
+    @FXML
+    private ListView<Cliente> lista;
+    private ObservableList<Cliente> itemList;
+     
+     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
+        itemList = FXCollections.observableArrayList();
         filterBox.getItems().addAll(filterList);
         
-    }    
+        //genero qualche cliente a caso per testare l'interfaccia
+        genItems();
+        lista.setItems(itemList);
+        lista.setCellFactory((ListView<Cliente> param) -> new ListCell<Cliente>() {
+            
+            private ImageView imageView = new ImageView("https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/32x32/plain/shape_circle.png");
+            
+            private Label textField = new Label("Text");
+            private Label lCitta= new Label("Text");
+            private Button btnModifica = new Button("Modifica");
+            private Button btnElimina = new Button("Elimina");
+            
+            private BorderPane bp = new BorderPane(imageView, textField, btnElimina, null,btnModifica);
+            private FlowPane fp = new FlowPane(Orientation.HORIZONTAL,imageView, textField, btnElimina, btnModifica, lCitta);
+
+            @Override
+            protected void updateItem(Cliente item, boolean empty) {
+                setPrefSize(1200, 180);
+                fp.setVgap(20);
+                fp.setHgap(14);
+                if (item == null || empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    setText(item.getNomeAz());
+                    btnElimina.setText("Elimina " + item.getNomeAz());
+                    textField.setText(item.getPIVA());
+                    lCitta.setText(item.getCitta());
+                    setGraphic(fp);
+                }
+            }
+        });
+
+    }
     
+        private void genItems() {
+        for(int i=0;i<10;i++){
+            itemList.add(new Cliente( "Azienda" +i, "idsfasdfs"," casasdfadf","tdsdfdf","IVA", "Genova","m", 3));
+        }
+    }
 }
